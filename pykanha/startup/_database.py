458396533b89da171af9e8cc8e851f6802b90ef5
Kanhagarg7 <162786866+Kanhagarg7@users.noreply.github.com>
2026-02-03 17:24:56 +0530

    Update _database.py

--- pykanha/startup/_database.py
+++ pykanha/startup/_database.py
@@ -108,7 +108,7 @@ class _BaseDatabase:
 
 
 class MongoDB(_BaseDatabase):
-    def __init__(self, key, dbname="kanhaDB"):
+    def __init__(self, key, dbname="kazamaDB"):
         self.dB = MongoClient(key, serverSelectionTimeoutMS=5000)
         self.db = self.dB[dbname]
         super().__init__()
